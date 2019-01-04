5d536d829d4b30cd15302427b6f4d071187a8605
Unknown <opticalfalicy@gmail.com>
2019-01-03 19:50:48 -0600

    Halfway thru Section 5. Error in api/posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Post model
+const Post = require('../../models/Post');
+
+// Profile model
+const Profile = require('../../models/Profile');
+
+// Validation
+const validatePostInput = require('../../validation/post')
 
 // @route GET api/posts/test
 // @desc Tests posts route
@@ -8,4 +19,78 @@ router.get('/test', (req, res) => res.json({
   msg: "Posts Works"
 }));
 
+// @route GET api/posts
+// @desc Gets posts
+// @access Public
+
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({
+      date: -1
+    })
+    .then(posts => {
+      res.json(posts)
+    })
+    .catch(err => res.status(404).json({
+      nopostsfound: 'No posts found'
+    }));
+})
+
+// @route GET api/posts/:id
+// @desc Gets posts by id
+// @access Public
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      res.json(post)
+    })
+    .catch(err => res.status(404).json({
+      nopostfound: 'No post found with that ID'
+    }));
+})
+// @route POST api/posts
+// @desc Create post
+// @access Private
+router.post('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  });
+
+  newPost.save().then(post => res.json(post));
+});
+
+// @route DELETE api/posts
+// @desc Delete post
+// @access Private
+router.delete('/:id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  Profile.findOne({
+      user: req.user.id
+    })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+              notauthorized: 'User not authorized'
+            });
+          }
+
+          // Delete
+          post.remove().then(() => json({
+            success: true
+          }));
+        })
+        .catch(err => res.status(400).json({
+          postnotfound: 'Post not found'
+        }));
+    });
+});
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,26 @@
+const Validator = require('validator');
+
+const isEmpty = require('./isEmpty');
+
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : '';
+
+  if (!Validator.isLength(data.text, {
+      min: 10,
+      max: 300
+    })) {
+    errors.text = 'Post must be between 10 and 300 characters.';
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = 'Text field required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
