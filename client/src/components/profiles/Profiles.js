93833b05199e28492fb29117d78756848db6b6f0
Unknown <opticalfalicy@gmail.com>
2019-02-15 03:49:03 -0600

    Started chapter 11

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Spinner from "../common/Spinner";
+import { getProfiles } from "../../actions/profileActions";
+
+class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+
+  render() {
+    const { profile, loading } = this.props.profile;
+    let profileItems;
+
+    if (profile === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profile.length > 0) {
+        profileItems = <h1>PROFILES HERE</h1>;
+      } else {
+        profileItems = <h4>No profiles found...</h4>;
+      }
+    }
+
+    return (
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">
+                <p className="lead text-center">Browse and connect with devs</p>
+                {profileItems}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
