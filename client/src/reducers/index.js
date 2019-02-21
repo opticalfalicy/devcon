aab689b54f7cf84e0f99384e99ef52d9cf1b1639
Unknown <opticalfalicy@gmail.com>
2019-02-21 16:29:16 -0600

    Started Chapter 12

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer.js";
 import profileReducer from "./profileReducer.js";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
