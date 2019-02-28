2acc1c92e3f16bff989c3d2b2c15eee4d7abd7cf
Unknown <opticalfalicy@gmail.com>
2019-02-28 03:07:15 -0600

    Fixed GET_POST bug

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -50,16 +50,16 @@ export const getPosts = () => dispatch => {
 export const getPost = id => dispatch => {
   dispatch(setPostLoading());
   axios
-    .get(`/api/post/${id}`)
+    .get(`/api/posts/${id}`)
     .then(res =>
       dispatch({
-        type: GET_POSTS,
+        type: GET_POST,
         payload: res.data
       })
     )
     .catch(err =>
       dispatch({
-        type: GET_POSTS,
+        type: GET_POST,
         payload: null
       })
     );
