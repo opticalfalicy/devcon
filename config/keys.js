0d3a28a0cea3b0d13286ef37e360aa9a9d47ac1f
swedishgoodbye <swedishgoodbye@gmail.com>
2019-03-13 03:04:02 -0500

    Prep to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://dev:c0nn3ct3d@ds023644.mlab.com:23644/devcon",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -24,12 +25,9 @@ const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 
 mongoose
-  .connect(
-    db,
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(db, {
+    useNewUrlParser: true
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -45,6 +43,18 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// SERVER static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
